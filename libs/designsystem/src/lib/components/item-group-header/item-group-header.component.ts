19b0b656a09bdcd931ccef3728d49b05adcb0a24
RasmusKjeldgaard <rkk@bankdata.dk>
2021-11-30 08:37:02 +0100

    ✨ Add item group header component

---
+++ libs/designsystem/src/lib/components/item-group-header/item-group-header.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'kirby-item-group-header',
+  templateUrl: './item-group-header.component.html',
+  styleUrls: ['./item-group-header.component.scss'],
+})
+export class ItemGroupHeaderComponent {}
