82118dd5510ae64b9f39ce7e5d435d2bd70a41fe
RasmusKjeldgaard <rkk@bankdata.dk>
2021-12-01 12:37:51 +0100

    ♻️ Rename item-group-header to section-header

--- libs/designsystem/src/lib/components/item-group-header/item-group-header.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'kirby-item-group-header',
-  templateUrl: './item-group-header.component.html',
-  styleUrls: ['./item-group-header.component.scss'],
-})
-export class ItemGroupHeaderComponent {}
